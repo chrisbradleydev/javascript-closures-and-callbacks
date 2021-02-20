a1d2022a5afa4bbf0523b0a21f191d05bc3d48a6
Chris Bradley <chris_bradley@sweetwater.com>
2021-02-20 14:49:17 -0500

    02-06

---
+++ src/02/06/index.js
@@ -0,0 +1,49 @@
+// Use Lodash Curry When Functions Return Functions
+
+import { curry } from 'lodash';
+
+const createTimeout = curry((time, listener) => {
+    const id = setTimeout(listener, time);
+    return () => {
+        clearTimeout(id);
+    };
+});
+
+const addListener = curry((element, eventType, listener) => {
+    const el = document.createElement(element);
+    el.innerHTML = 'click me';
+    el.addEventListener(eventType, listener);
+    document.body.appendChild(el);
+    return () => {
+        el.removeEventListener(eventType, listener);
+    };
+});
+
+const createInterval = curry((time, listener) => {
+    const id = setInterval(listener, time);
+    return () => {
+        clearInterval(id);
+    };
+});
+
+const addButtonListener = addListener('button');
+const addButtonClickListener = addButtonListener('click');
+const oneSecond = createInterval(1000);
+
+// broadcaster = function that accepts a listener
+const merge = curry((broadcaster1, broadcaster2, listener) => {
+    const cancel1 = broadcaster1(listener);
+    const cancel2 = broadcaster2(listener);
+
+    return () => {
+        cancel1();
+        cancel2();
+    };
+});
+
+const clickOrTick = merge(addButtonClickListener, oneSecond);
+const cancelClickOrTick = clickOrTick(() => {
+    console.log('click or tick');
+});
+
+// cancelClickOrTick();
