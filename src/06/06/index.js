192e47cf01eecc909317e1e55f25c1b359607a74
Christopher Bradley <ctb2oo5@gmail.com>
2021-02-27 21:17:24 -0500

    06-06

---
+++ src/06/06/index.js
@@ -0,0 +1,23 @@
+// Explaining the useListener Code
+
+import React from 'react';
+import { useBroadcaster, useListener } from '../../../lib/broadcasters';
+import { targetValue } from '../../../lib/operators';
+import { render } from 'react-dom';
+
+const div = document.createElement('div');
+div.id = 'root';
+document.body.appendChild(div);
+
+const App = () => {
+    const onInput = useListener();
+    const state = useBroadcaster(targetValue(onInput));
+    return (
+        <div>
+            <input type="text" onInput={onInput} />
+            <p>{state}</p>
+        </div>
+    );
+};
+
+render(<App />, document.querySelector('#root'));
