f40d28925422a178adb2b7675e79bd5a1ba4f8f7
Christopher Bradley <ctb2oo5@gmail.com>
2021-02-28 17:32:12 -0500

    07-01

---
+++ src/07/01/index.js
@@ -0,0 +1,68 @@
+// Build a Live Search Box with useBroadcaster and useListener in React
+
+import React from 'react';
+import { useBroadcaster, useListener } from '../../../lib/broadcasters';
+import { mapBroadcaster, map, targetValue, waitFor } from '../../../lib/operators';
+import { render } from 'react-dom';
+import { pipe } from 'lodash/fp';
+
+const openLibraryBaseUrl = 'https://openlibrary.org';
+
+const mapError = transform => broadcaster => listener => {
+    return broadcaster(value => {
+        if (value instanceof Error) {
+            listener(transform(value));
+            return;
+        }
+        listener(value);
+    });
+};
+
+const getUrl = url => listener => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetch(url, { signal })
+        .then(response => response.json())
+        .then(json => {
+            listener(json);
+        })
+        .catch(error => {
+            listener(error);
+        });
+
+    return () => {
+        controller.abort();
+    };
+};
+
+const div = document.createElement('div');
+div.id = 'root';
+document.body.appendChild(div);
+
+const App = () => {
+    const onInput = useListener();
+
+    const inputValue = targetValue(onInput);
+
+    const inputToBookSearch = pipe(
+        waitFor(500),
+        map(name => `${openLibraryBaseUrl}/search.json?q=${name}`),
+        mapBroadcaster(getUrl),
+        map(result => result.docs),
+    )(inputValue);
+
+    const books = useBroadcaster(inputToBookSearch, []);
+
+    return (
+        <div>
+            <input type="text" onInput={onInput} />
+            {books.map(book => (
+                <div key={book.key}>
+                    <a href={`${openLibraryBaseUrl}${book.key}`}>{book.title}</a>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+render(<App />, document.querySelector('#root'));
